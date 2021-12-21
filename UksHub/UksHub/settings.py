5a01c5149ff431d923481973118819d54db5f9f3
Dimitrije Zarkovic <dimitrije.zarkovic@vegait.rs>
2021-12-21 15:35:54 +0100

    Configuration update

--- UksHub/UksHub/settings.py
+++ UksHub/UksHub/settings.py
@@ -129,13 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# The root folder that provides static files for the deployment.
-# To put all static files here, run the following command:
-# python manage.py collectstatic --noinput
 STATIC_ROOT = '../static/'
 
 # SCSS dependency
 STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder'
 ]
 COMPRESS_PRECOMPILERS = (
@@ -145,6 +144,7 @@ COMPRESS_PRECOMPILERS = (
 COMPRESS_OFFLINE = True
 LIBSASS_OUTPUT_STYLE = 'compressed'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
