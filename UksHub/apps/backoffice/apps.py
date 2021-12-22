a0786992a6ca66741e066079c153f3b63eaef8ed
Dimitrije Zarkovic <dimitrije.zarkovic@vegait.rs>
2021-12-22 13:44:15 +0100

    [4-organize-django] - Organize django web application. Separate: settnings, configurations, applications, tamplates, satatic files

--- UksHub/apps/backoffice/apps.py
+++ UksHub/apps/backoffice/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HubConfig(AppConfig):
+class BackofficeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hub'
+    name = 'UksHub.apps.backoffice'
