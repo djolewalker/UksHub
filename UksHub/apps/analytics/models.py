19e7f9ff4426a2964cb8f25567e22717872f37bb
Dimitrije Zarkovic <dimitrije24000@gmail.com>
2022-01-05 10:54:51 +0100

    [1-class-diagram] Add session and visit from analytics app, PublicKey from gitcore app

--- UksHub/apps/analytics/models.py
+++ UksHub/apps/analytics/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.sessions.models import Session
 
 # Create your models here.
 class Visit(models.Model):
     path = models.CharField(max_length=240, default='')
-    responseCode = models.CharField(max_length=3, default='')
+    response_code = models.CharField(max_length=3, default='')
     time = models.DateTimeField()
     host = models.CharField(max_length=30)
     session_id = models.CharField(max_length=40, blank=True, null=True)
