f2ba8873f20d6d4f2223db16bbcc3dd68b1f7d80
Dimitrije <dimitrije.zarkovic@material-exchange.com>
2022-01-15 14:36:18 +0100

    Config file fix

--- UksHub/settings/dev.py
+++ UksHub/settings/dev.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = 'static'
 
 MEDIA_ROOT = BASE_DIR / 'user-files'
 MEDIA_URL = 'user-files/'
