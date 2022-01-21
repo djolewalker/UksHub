ddf3d3eabe10de3d9e686bda8339d669f3e2c23e
Dimitrije Zarkovic <dimitrije24000@gmail.com>
2022-01-21 13:09:59 +0100

    [bugfix] [55-fix-dev-git-repo] Variable USE_GIT_DEV added to test and production settings

--- UksHub/settings/test.py
+++ UksHub/settings/test.py
@@ -193,6 +193,7 @@ else:
 LOGIN_URL = "/login"
 
 GIT_REPOSITORIES = BASE_DIR / 'git-repos'
+USE_DEV_GIT = False
 
 GIT_ADMIN_SUPERUSER = 'random.user.admin'
 GIT_ADMIN = BASE_DIR / 'gitolite-admin'
