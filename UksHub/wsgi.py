403ffae03e7c98d13b2e38e0637c063b8a531a62
Dimitrije <dimitrije.zarkovic@material-exchange.com>
2022-01-25 22:27:30 +0100

    Config

--- UksHub/wsgi.py
+++ UksHub/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UksHub.settings.pg')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UksHub.settings.dev')
 
 application = get_wsgi_application()
